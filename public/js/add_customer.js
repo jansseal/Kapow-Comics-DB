b256513e823f2f98f2df5e661c2fe046b95cf2cb
Alexandra Janssen <jansseal@flip3.engr.oregonstate.edu>
2023-08-13 15:01:02 -0700

    Did the git add command for all new pages

---
+++ public/js/add_customer.js
@@ -0,0 +1,108 @@
+// Get the objects we need to modify
+let addCustomerForm = document.getElementById('add-customer-form-ajax');
+
+// Modify the objects we need
+addCustomerForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputCustomerName = document.getElementById("input-name");
+    let inputCustomerEmail = document.getElementById("input-email");
+    let inputCustomerPhone = document.getElementById("input-phone");
+
+    // Get the values from the form fields
+    let nameValue = inputCustomerName.value;
+    let emailValue = inputCustomerEmail.value;
+    let phoneValue = inputCustomerPhone.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        customer_name:  nameValue,
+        customer_email: emailValue,
+        customer_phone: phoneValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-customer-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputCustomerName.value = '';
+            inputCustomerEmail.value = '';
+            inputCustomerPhone.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// Customers
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("customers-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let nameCell = document.createElement("TD");
+    let emailCell = document.createElement("TD");
+    let phoneCell = document.createElement("TD");
+    let deleteCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idCell.innerText = newRow.customer_id;
+    nameCell.innerText = newRow.customer_name;
+    emailCell.innerText = newRow.customer_email;
+    phoneCell.innerText = newRow.customer_phone;
+
+    deleteCell = document.createElement("button");
+    deleteCell.innerHTML = "Delete";
+    deleteCell.onclick = function(){
+        deleteSaleDetails(newRow.customer_id);
+    };
+    
+    // Add the cells to the row 
+    row.appendChild(idCell);
+    row.appendChild(nameCell);
+    row.appendChild(emailCell);
+    row.appendChild(phoneCell);
+
+    // Add a row attribute so the deleteRow function can find a newly added row
+    row.setAttribute('data-value', newRow.customer_id);
+
+    // Add the row to the table
+    currentTable.appendChild(row);
+
+    // Find drop down menu, create a new option, fill data in the option (full name, id),
+    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
+    let selectMenu = document.getElementById("mySelect");
+    let option = document.createElement("option");
+    option.text = newRow.customer_id;
+    option.value = newRow.customer_id;
+    selectMenu.add(option);
+}
