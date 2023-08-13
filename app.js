a0e98cba1f4a4b1479c92de6b2b171419351790e
Alexandra Janssen <jansseal@flip3.engr.oregonstate.edu>
2023-08-13 14:17:40 -0700

    In the middle of implementing Add function for Customers page. Copied code from add_saleDetails and am up to line 25 in updating accordingly

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ PORT        = 36999;                 // Set a port number at the top so it's eas
 var db = require('./database/db-connector');
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
+const { isNull } = require('util');
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
@@ -122,6 +123,56 @@ app.post('/add-saleDetails-ajax', function(req, res)
             }
         })
     });
+
+app.post('/add-customer-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+        let customer_name = data.customer_name;
+        let customer_email = data.customer_email;
+
+        let customer_phone = data.customer_phone;
+        if (isNull(customer_phone))
+        {
+            customer_phone = 'NULL'
+        };
+
+        console.log("Data values:", customer_name, customer_email, customer_phone)
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Customers (customer_name, customer_email, customer_phone) VALUES (${customer_name}, ${customer_email}, ${customer_phone})`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM Customers;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
+    
 
 /*
     LISTENER
