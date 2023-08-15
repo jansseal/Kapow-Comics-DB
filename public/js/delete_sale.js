3eb0a79d96cf02e1ea222316e4b948389c18a2c4
Alexandra Janssen <jansseal@flip3.engr.oregonstate.edu>
2023-08-14 21:38:01 -0700

    Updated comments for both DELETE .js files

--- public/js/delete_sale.js
+++ public/js/delete_sale.js
@@ -1,3 +1,10 @@
+/*
+   Most of the following code has been modified from the Node.js Starter App.
+   Resources:
+   1. Node.js Starter App - https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main
+*/
+
+// Function to delete a sale using AJAX
 function deleteSale(sale_id) {
     let data = {
       sale_id: sale_id
@@ -7,22 +14,19 @@ function deleteSale(sale_id) {
     xhttp.open("DELETE", "/delete-sale-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
-    // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 204) {
-
-            // Add the new data to the table
+            // If the deletion is successful, remove the row from the table
             deleteRow(sale_id);
-
         }
         else if (xhttp.readyState == 4 && xhttp.status != 204) {
             console.log("There was an error with the input.")
         }
     }
-    // Send the request and wait for the response
     xhttp.send(JSON.stringify(data));
 }
   
+  // Function to delete a row from the sales table
   function deleteRow(sale_id){
       let table = document.getElementById("sales-table");
       for (let i = 0, row; row = table.rows[i]; i++) {
--- public/js/delete_saleDetails.js
+++ public/js/delete_saleDetails.js
@@ -1,3 +1,10 @@
+/*
+   Most of the following code has been modified from the Node.js Starter App.
+   Resources:
+   1. Node.js Starter App - https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main
+*/
+
+// Function to delete a sale's details using AJAX
 function deleteSaleDetails(invoice_id) {
     let data = {
       invoice_id: invoice_id
@@ -7,22 +14,19 @@ function deleteSaleDetails(invoice_id) {
     xhttp.open("DELETE", "/delete-saleDetails-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
-    // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 204) {
-
-            // Add the new data to the table
+            // If the deletion is successful, remove the row from the table
             deleteRow(invoice_id);
-
         }
         else if (xhttp.readyState == 4 && xhttp.status != 204) {
             console.log("There was an error with the input.")
         }
     }
-    // Send the request and wait for the response
     xhttp.send(JSON.stringify(data));
 }
   
+  // Function to delete a row from the sales details table
   function deleteRow(invoice_id){
       let table = document.getElementById("salesDetails-table");
       for (let i = 0, row; row = table.rows[i]; i++) {
